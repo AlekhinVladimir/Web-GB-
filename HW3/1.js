0eafad64916918c195b19cbd0e7adfb3d5e5f54e
AlekhinVladimir <Alehin.v.m@mail.com>
2023-06-16 10:05:32 +0400

    HW3

--- HW3/1.js
+++ HW3/1.js
@@ -1,4 +1,5 @@
 var celsius = prompt("Введите температуру в градусах Цельсия");
 var fahrenheit = Math.round((9 / 5) * celsius + 32);
+fahrenheit = Math.round(fahrenheit * 100) / 100;
 alert("Цельсий: " + celsius + ", Фаренгейт: " + fahrenheit);
 //
