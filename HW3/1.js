cee34d96f883c8028022bdfc814d343153104ca1
AlekhinVladimir <Alehin.v.m@mail.com>
2023-06-16 09:28:30 +0400

    HW3

---
+++ HW3/1.js
@@ -0,0 +1,4 @@
+var celsius = prompt("Введите температуру в градусах Цельсия");
+var fahrenheit = Math.round((9 / 5) * celsius + 32);
+alert("Цельсий: " + celsius + ", Фаренгейт: " + fahrenheit);
+//
